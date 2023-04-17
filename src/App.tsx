572de922ef34d9dfd669c91f6d2e397d8eb4d34a
wx <wongweixiangjulian@gmail.com>
2023-04-17 15:38:07 +0800

    Edit source code

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          This is WX's site
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
