a172fa2e6b0b32468da8adf8a42251b768fade26
Julian Wong Wei Xiang <wongweixiangjulian@gmail.com>
2025-04-10 14:11:12 +0800

    feat: add fallback

--- src/pages/UserProfile/ProfilePanel.tsx
+++ src/pages/UserProfile/ProfilePanel.tsx
@@ -6,7 +6,7 @@ const ProfilePanel = ({
   fullName,
   email,
   phoneNo,
-  profileImgUrl,
+  profileImgUrl = "",
 }: {
   fullName: string;
   email: string;
@@ -15,11 +15,16 @@ const ProfilePanel = ({
 }) => {
   return (
     <div className="flex flex-col min-w-[300px] p-5 grey-border">
-      <img
-        className="w-48 mx-auto mt-8 mb-12 rounded-full"
-        src={profileImgUrl}
-        alt="profile-picture"
-      />
+      {!!profileImgUrl ? (
+        <img
+          className="w-48 h-48 mx-auto mt-8 mb-12 rounded-full"
+          src={profileImgUrl}
+          alt="profile"
+        />
+      ) : (
+        <div className="w-48 h-48 mx-auto mt-8 mb-12 rounded-full bg-slate-300"></div>
+      )}
+
       <ProfileField label="Name" field={fullName} />
       <ProfileField label="Email" field={email} />
       <ProfileField label="Phone Number" field={phoneNo} />
